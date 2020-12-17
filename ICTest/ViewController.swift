6dae0bccc1c9d4344d39b50dcaf6f87c0158c385
Pedro Erazo <pedreus@gmail.com>
2020-12-17 09:12:45 -0500

    first commit

---
+++ ICTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ICTest
+//
+//  Created by Pedro Erazo Acosta on 17/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
